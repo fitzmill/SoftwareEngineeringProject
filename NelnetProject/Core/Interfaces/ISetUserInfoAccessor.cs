59149ba12b8bbac11818e5973cc35ae41dd63ff1
LHall383 <lahall1009@aol.com>
2018-02-07 20:40:48 -0600

    Created interface for the SetUserInfoAccessor

---
+++ NelnetProject/Core/Interfaces/ISetUserInfoAccessor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Interfaces
+{
+    /// <summary>
+    /// Stores information about users in the database.
+    /// </summary>
+    interface ISetUserInfoAccessor
+    {
+        /// <summary>
+        /// Creates a user record in the database
+        /// </summary>
+        /// <param name="user">The user to store in the database</param>
+        /// <returns>The id of the newly created user</returns>
+        int CreateUser(User user);
+
+        /// <summary>
+        /// Updates a user record in the database
+        /// </summary>
+        /// <param name="user"></param>
+        void UpdateUser(User user);
+
+    }
+}
