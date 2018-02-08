d07520f07d21096427a1c279e7fa8f72384497c1
markrnail <markrnail@gmail.com>
2018-02-07 20:42:34 -0600

    Created the Interfaces for GetPaymentInfoAccessor and GetUserInfoEngine.

---
+++ NelnetProject/Core/Interfaces/IGetPaymentInfoAccessor.cs
@@ -0,0 +1,17 @@
+﻿using Core.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Interfaces
+{
+    public interface IGetPaymentInfoAccessor
+    {
+        /// <summary>
+        /// Gets a user's payment info from Payment Spring with their user ID.
+        /// </summary>
+        /// <param name="customerID"></param>
+        /// <returns>A user's payment info provided by Payment Spring/returns>
+        PaymentDTO GetPaymentInfoForUser(int customerID);
+    }
+}
