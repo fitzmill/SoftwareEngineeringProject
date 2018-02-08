c30bc170f8207b4eeafb0da07dd1d1d43d9db79a
fitzmill <sfitzmill@gmail.com>
2018-02-07 20:46:56 -0600

    Added class level descriptions.

--- NelnetProject/Core/Interfaces/IGetTransactionAccessor.cs
+++ NelnetProject/Core/Interfaces/IGetTransactionAccessor.cs
@@ -4,6 +4,9 @@ using System.Text;
 
 namespace Core.Interfaces
 {
+    /// <summary>
+    /// Accessor for getting transactions
+    /// </summary>
     public interface IGetTransactionAccessor
     {
         /// <summary>
--- NelnetProject/Core/Interfaces/ISetTransactionAccessor.cs
+++ NelnetProject/Core/Interfaces/ISetTransactionAccessor.cs
@@ -4,6 +4,9 @@ using System.Text;
 
 namespace Core.Interfaces
 {
+    /// <summary>
+    /// Accessor for setting transaction information
+    /// </summary>
     public interface ISetTransactionAccessor
     {
         /// <summary>
