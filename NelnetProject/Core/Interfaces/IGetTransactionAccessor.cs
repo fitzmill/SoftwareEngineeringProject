e69b3209512f11dd4d1e21fdcc621e6132c478b0
fitzmill <sfitzmill@gmail.com>
2018-02-07 20:41:10 -0600

    Created initial transaction accessor interfaces.

---
+++ NelnetProject/Core/Interfaces/IGetTransactionAccessor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Interfaces
+{
+    public interface IGetTransactionAccessor
+    {
+        /// <summary>
+        /// Gets a list of all transactions from the database for a user based on their userID
+        /// </summary>
+        /// <param name="userID"></param>
+        /// <returns>A list of transactions for the user</returns>
+        List<Transaction> GetTransactionsForUser(int userID);
+
+        /// <summary>
+        /// Gets a list of transactions from the database for all users that were charged in a given date range
+        /// </summary>
+        /// <param name="startTime"></param>
+        /// <param name="endTime"></param>
+        /// <returns>List of transactions that occurred in the date range</returns>
+        List<Transaction> GetTransactionsForDateRange(DateTime startTime, DateTime endTime);
+
+        /// <summary>
+        /// Gets all uncharged transactions from the database
+        /// </summary>
+        /// <returns>List of all uncharged transactions</returns>
+        List<Transaction> GetAllUnchargedTransactions();
+
+        /// <summary>
+        /// Gets the most recent transaction for a user from the database based on their userID
+        /// </summary>
+        /// <param name="userID"></param>
+        /// <returns>Most recent transaction for user</returns>
+        Transaction GetTransactionByUserID(int userID);
+    }
+}
---
+++ NelnetProject/Core/Interfaces/ISetTransactionAccessor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Interfaces
+{
+    public interface ISetTransactionAccessor
+    {
+        /// <summary>
+        /// Adds a transaction to the database
+        /// </summary>
+        /// <param name="transaction"></param>
+        void AddTransaction(Transaction transaction);
+
+        /// <summary>
+        /// Updates a transaction that's already in the database
+        /// </summary>
+        /// <param name="transaction"></param>
+        void UpdateTransaction(Transaction transaction);
+
+
+    }
+}
