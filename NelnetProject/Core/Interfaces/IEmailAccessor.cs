4e1df989202ca9c7a85822b2c3e4b57d4a4f0d75
Joe Cowman <joe.cowman@huskers.unl.edu>
2018-02-07 20:20:17 -0600

    IEmailAccessor

---
+++ NelnetProject/Core/Interfaces/IEmailAccessor.cs
@@ -0,0 +1,19 @@
+﻿using Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Interfaces
+{
+    /// <summary>
+    /// Interface for the accessor to the email sending service.
+    /// </summary>
+    interface IEmailAccessor
+    {
+        /// <summary>
+        /// Sends an email notification.
+        /// </summary>
+        /// <param name="emailNotification"></param>
+        void SendEmail(EmailNotification emailNotification);
+    }
+}
