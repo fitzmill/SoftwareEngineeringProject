9a152cc3e3971194689334092c139344e48d44f5
LHall383 <lahall1009@aol.com>
2018-02-09 15:21:19 -0600

    Updated method signature to accept payment info for the user.

--- NelnetProject/Core/Interfaces/ISetUserInfoEngine.cs
+++ NelnetProject/Core/Interfaces/ISetUserInfoEngine.cs
@@ -13,12 +13,20 @@ namespace Core.Interfaces
         /// Creates a user record in the database and an associated customer in paymentSpring
         /// </summary>
         /// <param name="user">The user model to store</param>
-        void CreateUser(User user);
+        /// <param name="cardNumber">A credit card number</param>
+        /// <param name="expirationYear">Year of expiration</param>
+        /// <param name="expirationMonth">Month of expiration</param>
+        /// <param name="CSC">Security code</param>
+        void CreateUser(User user, string cardNumber, string expirationYear, string expirationMonth, string CSC);
 
         /// <summary>
         /// Updates a user record in the database and the associated customer in paymentSpring
         /// </summary>
         /// <param name="user">The user model to update</param>
-        void UpdateUser(User user);
+        /// <param name="cardNumber">A credit card number</param>
+        /// <param name="expirationYear">Year of expiration</param>
+        /// <param name="expirationMonth">Month of expiration</param>
+        /// <param name="CSC">Security code</param>
+        void UpdateUser(User user, string cardNumber, string expirationYear, string expirationMonth, string CSC);
     }
 }
