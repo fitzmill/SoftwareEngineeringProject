082c1f98ea0a7d3fce26b030a59691c5e48060f2
Joe Cowman <joe.cowman@huskers.unl.edu>
2018-02-09 15:04:48 -0600

    IPaymentEngine interface

---
+++ NelnetProject/Core/Interfaces/IPaymentEngine.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Interfaces
+{
+    /// <summary>
+    /// Interface for engine that charges payments to users.
+    /// </summary>
+    interface IPaymentEngine
+    {
+        /// <summary>
+        /// Charges a list of payments and returns the results.
+        /// </summary>
+        /// <param name="charges">List of unsettled transactions to charge.</param>
+        /// <returns>Results of all the charges.</returns>
+        List<Transaction> ChargePayments(List<Transaction> charges);
+    }
+}
