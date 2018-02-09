ecbb3140a6a509bf34a69fa5e6d083e811ba3a5a
fitzmill <sfitzmill@gmail.com>
2018-02-09 14:52:52 -0600

    Made IGetTransactionEngine

---
+++ NelnetProject/Core/Interfaces/IGetTransactionEngine.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Interfaces
+{
+    public interface IGetTransactionEngine
+    {
+        /// <summary>
+        /// Gets a list of all transactions from the accessor for a user based on their userID
+        /// </summary>
+        /// <param name="userID"></param>
+        /// <returns>A list of transactions for the user</returns>
+        List<Transaction> GetTransactionsForUser(int userID);
+
+        /// <summary>
+        /// Gets a list of transactions from the accesesor for all users that were charged in a given date range
+        /// </summary>
+        /// <param name="startTime"></param>
+        /// <param name="endTime"></param>
+        /// <returns>List of transactions that occurred in the date range</returns>
+        List<Transaction> GetTransactionsForDateRange(DateTime startTime, DateTime endTime);
+
+        /// <summary>
+        /// Gets all transactions that have not been successfully charged
+        /// </summary>
+        /// <returns>List of all unsettled transactions</returns>
+        List<Transaction> GetAllUnsettledTransactions();
+
+        /// <summary>
+        /// Gets the most recent transaction for a user from the accessor based on their userID
+        /// </summary>
+        /// <param name="userID"></param>
+        /// <returns>Most recent transaction for user</returns>
+        Transaction GetTransactionByUserID(int userID);
+    }
+}
