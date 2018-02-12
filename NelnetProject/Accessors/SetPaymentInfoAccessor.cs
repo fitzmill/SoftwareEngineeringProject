8def664838fab16479f68de6fe3c6005853a1991
LHall383 <lahall1009@aol.com>
2018-02-12 15:16:27 -0600

    Implemented structure of SetUserInfoAccessor and SetPaymentInfoAccessor

---
+++ NelnetProject/Accessors/SetPaymentInfoAccessor.cs
@@ -0,0 +1,25 @@
+﻿using Core.DTOs;
+using Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Accessors
+{
+    class SetPaymentInfoAccessor : ISetPaymentInfoAccessor
+    {
+        public string CreateCustomer(UserPaymentInfoDTO customerInfo)
+        {
+            //create a customer through the paymentSpring API
+            //return the generated customerID
+            throw new NotImplementedException();
+        }
+
+        //TODO: we may need to pass in the customerID here in order to know which customer to update (unless our DTO has everything that paymentSpring needs)
+        public void UpdateCustomer(UserPaymentInfoDTO customerInfo) 
+        {
+            //update the customer information in paymentSpring
+            throw new NotImplementedException();
+        }
+    }
+}
