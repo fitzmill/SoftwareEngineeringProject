99981dbcbd6b64520249d8a1621238afd7d033f8
markrnail <markrnail@gmail.com>
2018-02-12 19:11:11 -0600

    Added GetPaymentInfoAccessor, GetUserInfoAccessor, GetUserInfoEngine

---
+++ NelnetProject/Engines/GetUserInfoEngine.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core;
+using Core.Interfaces;
+using Core.DTOs;
+
+namespace Engines
+{
+    class GetUserInfoEngine : IGetUserInfoEngine
+    {
+        // Checks to see if a given email matches with an email in the database.
+        public bool EmailExists(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        // Validates the login of a user given an email and a password.
+        public bool ValidateLoginInfo(string email, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        // Gets a user's information when given a user's ID.
+        public User GetUserInfoByID(int userID)
+        {
+            throw new NotImplementedException();
+        }
+
+        // Gets a user's information when given an email.
+        public User GetUserInfoByEmail(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        // Gets a user's payment info from Payment Spring with their customer ID.
+        public UserPaymentInfoDTO GetPaymentInfoForUser(string customerID)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
