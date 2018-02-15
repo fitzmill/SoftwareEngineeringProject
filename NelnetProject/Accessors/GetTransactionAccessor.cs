5859d7287a370546a09878488559623b33b6a8c2
fitzmill <sfitzmill@gmail.com>
2018-02-15 12:37:04 -0600

    Added comments explaining code

--- NelnetProject/Accessors/GetTransactionAccessor.cs
+++ NelnetProject/Accessors/GetTransactionAccessor.cs
@@ -17,26 +17,38 @@ namespace Accessors
         //Executes a stored procedure in the database for getting all Transactions with userID as a parameter
         public List<Transaction> GetAllTransactionsForUser(int userID)
         {
+            //this is the format for executing stored procedures in SQL
             string query = "[dbo].[GetAllTransactionsForUser] @UserID=@passedInUserID";
 
+            //initialize return object
             var result = new List<Transaction>();
 
+            //this makes the connection, but doesn't open it. The connection will automatically be disposed of at the end of the using braces
             using (SqlConnection conn = new SqlConnection(connectionString))
             {
+                //makes the SQL command with the query string and connection object
                 SqlCommand command = new SqlCommand(query, conn);
+
+                //replaces the @passedInUserID that's in the query string with the integer userID
                 command.Parameters.AddWithValue("@passedInUserID", userID);
 
                 conn.Open();
+
+                //what actually executes the command
                 var reader = command.ExecuteReader();
 
+                //using a while loop since I'm expecting multiple rows from the query
+                //each iteration will contain one row from the Transaction table
                 while (reader.Read())
                 {
+                    //Adds the new transaction to the result set
                     result.Add(new Transaction()
                     {
                         TransactionID = reader.GetInt32(0),
                         UserID = reader.GetInt32(1),
                         AmountCharged = reader.GetDouble(2),
                         DateDue = reader.GetDateTime(3),
+                        //one line if statements have to return the same type, so nullable simple types need to be explicitly casted
                         DateCharged = reader.IsDBNull(4) ? (DateTime?)null : reader.GetDateTime(4),
                         ProcessState = (ProcessState)reader.GetByte(5),
                         ReasonFailed = (ReasonFailed?)(reader.IsDBNull(6) ? (int?)null : reader.GetByte(6))
--- NelnetProject/Web/Controllers/AdminController.cs
+++ NelnetProject/Web/Controllers/AdminController.cs
@@ -10,22 +10,30 @@ using System.Web.Http;
 
 namespace Web.Controllers
 {
+    //this is the route in the url for this set of APIs. So the url would be localhost:port/api/admin
     [RoutePrefix("api/admin")]
     public class AdminController : ApiController
     {
         IGetTransactionEngine getTransactionEngine;
+        //this object gets injected as a dependency by Unity
         public AdminController(IGetTransactionEngine getTransactionEngine)
         {
             this.getTransactionEngine = getTransactionEngine;
         }
 
         // GET api/admin/GetAllTransactionsForUser/5
+        //This is a get request with the above route. The 5 at the end of the example is an example userID
+        [HttpGet]
         [Route("GetAllTransactionsForUser/{userID}")]
-        public List<Transaction> GetAllTransactionsForUser(string userID)
+        public IHttpActionResult GetAllTransactionsForUser(string userID)
         {
             int intUserID = 0;
-            int.TryParse(userID, out intUserID);
-            return getTransactionEngine.GetAllTransactionsForUser(intUserID);
+            //Tries to convert the parameter to an int
+            if (!int.TryParse(userID, out intUserID))
+            {
+                return BadRequest("Could not parse userID into an integer");
+            }
+            return Ok(getTransactionEngine.GetAllTransactionsForUser(intUserID));
         }
     }
 }
