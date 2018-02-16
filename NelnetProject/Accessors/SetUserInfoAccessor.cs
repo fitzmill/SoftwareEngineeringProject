8e2bfebab08c7dbde5bf357ff0db836a59765469
LHall383 <lahall1009@aol.com>
2018-02-15 22:41:06 -0600

    Implemented SetUserInfoAccessor utilizing stored procedures from the database.

--- NelnetProject/Accessors/SetUserInfoAccessor.cs
+++ NelnetProject/Accessors/SetUserInfoAccessor.cs
@@ -2,47 +2,156 @@
 using Core.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Text;
 
 namespace Accessors
 {
     public class SetUserInfoAccessor : ISetUserInfoAccessor
     {
-        public void InsertPersonalInfo(User user)
+        string connectionString;
+
+        public SetUserInfoAccessor(string connectionString)
         {
-            //add the personal record from the user to the database
-            //store the auto-generated database id in the user model
-            throw new NotImplementedException();
+            this.connectionString = connectionString;
         }
 
+        //insert a new record into the user table
+        public void InsertPersonalInfo(User user)
+        { 
+            //specifies stored query and parameters
+            string query = "[dbo].[InsertPersonalInfo] @FirstName=_FirstName, @LastName=_LastName, @Email=_Email, @Hashed=_Hashed, @Salt=_Salt, @PaymentPlan=_PaymentPlan, @UserType=_UserType, @CustomerID=_CustomerID";
+
+            //add the personal record from the user to the database
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+
+                //fill in parameters
+                command.Parameters.AddWithValue("_FirstName", user.FirstName);
+                command.Parameters.AddWithValue("_LastName", user.FirstName);
+                command.Parameters.AddWithValue("_Email", user.FirstName);
+                command.Parameters.AddWithValue("_Hashed", user.FirstName);
+                command.Parameters.AddWithValue("_Salt", user.FirstName);
+                command.Parameters.AddWithValue("_PaymentPlan", user.FirstName);
+                command.Parameters.AddWithValue("_UserType", user.FirstName);
+                command.Parameters.AddWithValue("_CustomerID", user.FirstName);
+
+                //open connection
+                connection.Open();
+
+                //execute command
+                var reader = command.ExecuteReader();
+
+                //read the id of the user that has just been created
+                user.UserID = reader.GetInt32(0);
+            }
+        }
+
+        //use the id from the user model to update the record asociated with the user
         public void UpdatePersonalInfo(User user)
         {
-            //use the id from the user model to update the record asociated with the user
-            throw new NotImplementedException();
+            string query = "[dbo].[UpdatePersonalInfo] @ID=_ID, @FirstName=_FirstName, @LastName=_LastName, @Email=_Email, @Hashed=_Hashed, @Salt=_Salt, @PaymentPlan=_PaymentPlan, @UserType=_UserType, @CustomerID=_CustomerID";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+
+                //fill in parameters
+                command.Parameters.AddWithValue("_ID", user.UserID);
+                command.Parameters.AddWithValue("_FirstName", user.FirstName);
+                command.Parameters.AddWithValue("_LastName", user.FirstName);
+                command.Parameters.AddWithValue("_Email", user.FirstName);
+                command.Parameters.AddWithValue("_Hashed", user.FirstName);
+                command.Parameters.AddWithValue("_Salt", user.FirstName);
+                command.Parameters.AddWithValue("_PaymentPlan", user.FirstName);
+                command.Parameters.AddWithValue("_UserType", user.FirstName);
+                command.Parameters.AddWithValue("_CustomerID", user.FirstName);
+
+                //open connection
+                connection.Open();
+
+                //execute command
+                command.ExecuteReader();
+            }
         }
 
+        //remove all personal data from the database associated with the userID
         public void DeletePersonalInfo(int userID)
         {
-            //remove all personal data from the database associated with the userID
-            throw new NotImplementedException();
+            string query = "[dbo].[DeletePersonalInfo] @ID=_ID";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+
+                command.Parameters.AddWithValue("_ID", userID);
+
+                connection.Open();
+
+                command.ExecuteReader();
+            }
         }
 
+        //add a new student to the database associated with the userID
         public void InsertStudentInfo(int userID, Student student)
         {
-            //add a new student to the database associated with the userID
-            throw new NotImplementedException();
+            string query = "[dbo].[InsertStudentInfo] @FirstName=_FirstName, @LastName=_LastName, @Grade=_Grade, @UserID=_UserID";
+            
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+
+                command.Parameters.AddWithValue("_FirstName", student.FirstName);
+                command.Parameters.AddWithValue("_LastName", student.LastName);
+                command.Parameters.AddWithValue("_Grade", student.Grade);
+                command.Parameters.AddWithValue("_UserID", userID);
+
+                connection.Open();
+
+                var reader = command.ExecuteReader();
+
+                student.StudentID = reader.GetInt32(0);
+            }
         }
 
+        //update a student in the database with the information provided
         public void UpdateStudentInfo(Student student)
         {
-            //update a student in the database with the information provided
-            throw new NotImplementedException();
+            string query = "[dbo].[UpdateStudentInfo] @ID=_ID, @FirstName=_FirstName, @LastName=_LastName, @Grade=_Grade";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+
+                command.Parameters.AddWithValue("_ID", student.StudentID);
+                command.Parameters.AddWithValue("_FirstName", student.FirstName);
+                command.Parameters.AddWithValue("_LastName", student.LastName);
+                command.Parameters.AddWithValue("_Grade", student.Grade);
+
+                connection.Open();
+
+                command.ExecuteReader();
+            }
         }
 
+        //remove data associated with the particular studentID
         public void DeleteStudentInfo(int studentID)
         {
-            //remove data associated with the particular studentID
-            throw new NotImplementedException();
+            string query = "[dbo].[DeleteStudentInfo] @ID=_ID";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+
+                command.Parameters.AddWithValue("_ID", studentID);
+
+                connection.Open();
+
+                command.ExecuteReader();
+            }
         }
     }
 }
