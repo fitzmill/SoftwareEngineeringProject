59066826cdc51b98ab1e8c1e9782ccc113cbe3d3
Joe Cowman <joe.cowman@huskers.unl.edu>
2018-02-21 20:35:36 -0600

    UpdateTransaction method

--- NelnetProject/Accessors/SetTransactionAccessor.cs
+++ NelnetProject/Accessors/SetTransactionAccessor.cs
@@ -33,6 +33,7 @@ namespace Accessors
                 command.Parameters.Add(new SqlParameter("@ReasonFailed", transaction.ReasonFailed));
 
                 command.CommandType = CommandType.StoredProcedure;
+
                 con.Open();
 
                 var reader = command.ExecuteReader();
@@ -48,7 +49,30 @@ namespace Accessors
         //Executes a stored procedure to update a transaction with all of the Transaction object's properties as parameters
         public void UpdateTransaction(Transaction transaction)
         {
-            throw new NotImplementedException();
+            string query = "[dbo].[UpdateTransaction]";
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(query, con);
+                command.Parameters.Add(new SqlParameter("@TransactionID", transaction.TransactionID));
+                command.Parameters.Add(new SqlParameter("@UserID", transaction.UserID));
+                command.Parameters.Add(new SqlParameter("@AmountCharged", transaction.AmountCharged));
+                command.Parameters.Add(new SqlParameter("@DateDue", transaction.DateDue));
+                command.Parameters.Add(new SqlParameter("@DateCharged", transaction.DateCharged));
+                command.Parameters.Add(new SqlParameter("@ProcessState", transaction.ProcessState));
+                command.Parameters.Add(new SqlParameter("@ReasonFailed", transaction.ReasonFailed));
+
+                command.CommandType = CommandType.StoredProcedure;
+
+                con.Open();
+
+                int rowsAffected = command.ExecuteNonQuery();
+
+                if (rowsAffected != 1)
+                {
+                    throw new Exception("Incorrect number of rows affected: " + rowsAffected);
+                }
+
+            }
         }
     }
 }
