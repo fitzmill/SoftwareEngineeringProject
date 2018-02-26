7db5123c94fa4ff8c04dcd9089103a0a51aa4ccb
Sean Fitzgerald <sfitzmill@gmail.com>
2018-02-26 14:37:53 -0600

    Added GetTransactionsForDateRange api

---
+++ NelnetProject/Core/DTOs/DateRangeDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DTOs
+{
+    public class DateRangeDTO
+    {
+        public DateDTO StartDate { get; set; }
+
+        public DateDTO EndDate { get; set; }
+    }
+}
