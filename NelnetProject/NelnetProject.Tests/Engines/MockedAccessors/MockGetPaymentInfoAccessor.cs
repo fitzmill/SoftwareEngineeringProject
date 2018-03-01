edeb52f5a89a7b65a1d26c90397c27e505db02b6
markrnail <markrnail@gmail.com>
2018-03-01 11:59:37 -0600

    Finished GetUserInfoEngine Tests

---
+++ NelnetProject/NelnetProject.Tests/Engines/MockedAccessors/MockGetPaymentInfoAccessor.cs
@@ -0,0 +1,44 @@
+﻿using Core.DTOs;
+using Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NelnetProject.Tests.Engines.MockedAccessors
+{
+    public class MockGetPaymentInfoAccessor : IGetPaymentInfoAccessor
+    {
+        public List<UserPaymentInfoDTO> MockPaymentSpring = new List<UserPaymentInfoDTO>()
+        {
+            new UserPaymentInfoDTO()
+            {
+                CustomerID = "fed123",
+                Company = "Martwall",
+                FirstName = "John",
+                LastName = "Smith",
+                StreetAddress1 = "1223 West St",
+                StreetAddress2 = "Apt. 3",
+                City = "Missouri City",
+                State = "Kansas",
+                Zip = "67208",
+                CardNumber = 1123,
+                ExpirationYear = 2025,
+                ExpirationMonth = 6,
+                CardType = "visa"
+            }
+        };
+
+        public UserPaymentInfoDTO GetPaymentInfoForCustomer(string customerID)
+        {
+            if (customerID.Equals(MockPaymentSpring[0].CustomerID))
+            {
+                return MockPaymentSpring[0];
+            } else
+            {
+                return null;
+            }
+        }
+    }
+}
--- NelnetProject/NelnetProject.Tests/Engines/TestGetUserInfoEngine.cs
+++ NelnetProject/NelnetProject.Tests/Engines/TestGetUserInfoEngine.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Core.Interfaces;
+using Engines;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NelnetProject.Tests.Engines.MockedAccessors;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +13,89 @@ namespace NelnetProject.Tests.Engines
     [TestClass]
     public class TestGetUserInfoEngine
     {
+        IGetUserInfoEngine getUserInfoEngine;
+        IGetPaymentInfoAccessor getPaymentInfoAccessor;
+        IGetUserInfoAccessor getUserInfoAccessor;
+        public TestGetUserInfoEngine()
+        {
+            getPaymentInfoAccessor = new MockGetPaymentInfoAccessor();
+            getUserInfoAccessor = new MockGetUserInfoAccessor();
+            getUserInfoEngine = new GetUserInfoEngine(getPaymentInfoAccessor, getUserInfoAccessor);
+        }
 
+        [TestMethod]
+        public void TestEmailExistsTrue()
+        {
+            string email = "johnsmith@gmail.com";
+            Assert.AreEqual(true, getUserInfoEngine.EmailExists(email));
+        }
+
+        [TestMethod]
+        public void TestEmailExistsFalse()
+        {
+            string email = "thisisnotanemail@gmail.com";
+            Assert.AreEqual(false, getUserInfoEngine.EmailExists(email));
+        }
+
+        [TestMethod]
+        public void TestValidateLoginInformationFalse()
+        {
+            string email = "johnsmith@gmail.com";
+            string password = "majlehlasg";
+            Assert.AreEqual(false, getUserInfoEngine.ValidateLoginInfo(email, password));
+        }
+
+        [TestMethod]
+        public void TestValidateLoginInformationTrue()
+        {
+            string email = "johnsmith@gmail.com";
+            string password = "password";
+            Assert.AreEqual(true, getUserInfoEngine.ValidateLoginInfo(email, password));
+        }
+
+        [TestMethod]
+        public void TestGetUserInfoByIDWithCorrectID()
+        {
+            int userID = 1;
+            string firstName = "John";
+            Assert.AreEqual(firstName, getUserInfoEngine.GetUserInfoByID(userID).FirstName);
+        }
+
+        [TestMethod]
+        public void TestGetUserInfoByIDWithoutCorrectID()
+        {
+            int userID = 23;
+            Assert.AreEqual(null, getUserInfoEngine.GetUserInfoByID(userID));
+        }
+
+        [TestMethod]
+        public void TestGetUserInfoByEmailWithCorrectEmail()
+        {
+            string email = "johnsmith@gmail.com";
+            string firstName = "John";
+            Assert.AreEqual(firstName, getUserInfoEngine.GetUserInfoByEmail(email).FirstName);
+        }
+
+        [TestMethod]
+        public void TestGetUserInfoByEmailWithoutCorrectEmail()
+        {
+            string email = "thisemailisntanemail@gmail.com";
+            Assert.AreEqual(null, getUserInfoEngine.GetUserInfoByEmail(email));
+        }
+
+        [TestMethod]
+        public void TestGetPaymentInfoForUserWithCorrectCustomerID()
+        {
+            string customerID = "fed123";
+            string firstName = "John";
+            Assert.AreEqual(firstName, getUserInfoEngine.GetPaymentInfoForUser(customerID).FirstName);
+        }
+
+        [TestMethod]
+        public void TestGetPaymentInfoForUserWithoutCorrectCustomerID()
+        {
+            string customerID = "thisisnotvalid";
+            Assert.AreEqual(null, getUserInfoEngine.GetPaymentInfoForUser(customerID));
+        }
     }
 }
