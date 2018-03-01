b6024de44e8e515ed571e5f43c97da0f051a5481
LHall383 <lahall1009@aol.com>
2018-02-28 23:04:18 -0600

    Created DeleteCustomer method.

--- NelnetProject/Engines/SetUserInfoEngine.cs
+++ NelnetProject/Engines/SetUserInfoEngine.cs
@@ -8,58 +8,57 @@ namespace Engines
 {
     public class SetUserInfoEngine : ISetUserInfoEngine
     {
+        //make a call to the SetPaymentInfoAccessor to add the payment info to paymentSpring and return the generated customerID
         public string InsertPaymentInfo(UserPaymentInfoDTO userPaymentInfo)
         {
-            //make a call to the SetPaymentInfoAccessor to add the payment info to paymentSpring
-            //return the generated customerID
             throw new NotImplementedException();
         }
 
+        //make a call to the SetPaymentInfoAccessor to update the payment info associated with the customerID
         public void UpdatePaymentInfo(string customerID, UserPaymentInfoDTO userPaymentInfo)
         {
-            //make a call to the SetPaymentInfoAccessor to update the payment info associated with the customerID
             throw new NotImplementedException();
         }
 
+        //make a call to the SetPaymentInfoAccessor to delete the payment info associated with the customerID
         public void DeletePaymentInfo(string customerID)
         {
-            //make a call to the SetPaymentInfoAccessor to delete the payment info associated with the customerID
             throw new NotImplementedException();
         }
 
+        //make a call to the SetUserInfoAccessor to add a new record to the database
         public void InsertPersonalInfo(User user)
-        {
-            //make a call to the SetUserInfoAccessor to add a new record to the database
+        { 
             throw new NotImplementedException();
         }
 
+        //make a call to the SetUserInfoAccessor that will update the database record associated with the user
         public void UpdatePersonalInfo(User user)
         {
-            //make a call to the SetUserInfoAccessor that will update the database record associated with the user
             throw new NotImplementedException();
         }
 
+        //make a call to the SetUserInfoAccessor to delete the personal information associated with the userID
         public void DeletePersonalInfo(int userID)
         {
-            //make a call to the SetUserInfoAccessor to delete the personal information associated with the userID
             throw new NotImplementedException();
         }
 
+        //make a call to the SetUserInfoAccessor to insert new students into the database to be associated with the userID
         public void InsertStudentInfo(int userID, List<Student> students)
         {
-            //make a call to the SetUserInfoAccessor to insert new students into the database to be associated with the userID
             throw new NotImplementedException();
         }
 
+        //make a call to the SetUserInfoAccessor to update the student records associated with the list of students
         public void UpdateStudentInfo(List<Student> students)
         {
-            //make a call to the SetUserInfoAccessor to update the student records associated with the list of students
             throw new NotImplementedException();
         }
 
+        //make a call to the SetUserInfoAcessor to delete the student information associated with the studentIDs
         public void DeleteStudentInfo(List<int> studentIDs)
-        {
-            //make a call to the SetUserInfoAcessor to delete the student information associated with the studentIDs
+        { 
             throw new NotImplementedException();
         }
     }
