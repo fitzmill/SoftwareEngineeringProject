304a1ec4a0d611593089a0f74459bed7969a2033
markrnail <markrnail@gmail.com>
2018-03-05 21:59:48 -0600

    Changed order of expected an output in assert.

--- NelnetProject/NelnetProject.Tests/Engines/Utils/TestPasswordUtils.cs
+++ NelnetProject/NelnetProject.Tests/Engines/Utils/TestPasswordUtils.cs
@@ -17,7 +17,7 @@ namespace NelnetProject.Tests.Engines.Utils
             string password = "password";
             string salt = "l1u2c3a4s5";
             string testHash = "78b10e2cb3ec22bffea25bad2a1c02cbe4b7b587b46d0dd8d6af1c170910a3b1";
-            Assert.AreEqual(PasswordUtils.HashPasswords(password, salt), testHash);
+            Assert.AreEqual(testHash, PasswordUtils.HashPasswords(password, salt));
         }
     }
 }
