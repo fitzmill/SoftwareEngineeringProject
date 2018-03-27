1c5a259d3a2a6d5188bd9b892099ae0a054a955e
LHall383 <lahall1009@aol.com>
2018-03-26 23:05:52 -0500

    Got a start on the account creation page

--- NelnetProject/Web/Nelnet_UI/source/AccountCreationComponent/account-creation-component.js
+++ NelnetProject/Web/Nelnet_UI/source/AccountCreationComponent/account-creation-component.js
@@ -1,1 +1,13 @@
-﻿
+﻿require('./account-creation-component.scss');
+
+ko.components.register('account-creation-component', {
+    viewModel: function (params) {
+        let vm = this;
+
+        vm.text = ko.observable("hi");
+
+        return vm;
+    },
+
+    template: require('./account-creation-component.html')
+});
