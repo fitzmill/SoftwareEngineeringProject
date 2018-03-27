da95b6b19cd01d9e45cef8d06397df7ab39b54a8
LHall383 <lahall1009@aol.com>
2018-03-26 21:28:25 -0500

    Added Insert methods to AccountController

--- NelnetProject/Web/Controllers/AccountController.cs
+++ NelnetProject/Web/Controllers/AccountController.cs
@@ -49,6 +49,24 @@ namespace Web.Controllers
             setUserInfoEngine.UpdatePersonalInfo(user);
             return Ok();
         }
+        [HttpPost]
+        [Route("InsertPersonalInfo")]
+        public IHttpActionResult InsertPersonalInfo(User user)
+        {
+            if (user == null || user.FirstName == null || user.LastName == null || user.CustomerID == null || user.Email == null || user.Hashed == null || user.Salt == null)
+            {
+                return BadRequest("One or more required objects was not included in the request body.");
+            }
+            foreach (Student s in user.Students)
+            {
+                if (s.FirstName == null || s.LastName == null)
+                {
+                    return BadRequest("One or more required objects was not included in the request body.");
+                }
+            }
+            setUserInfoEngine.InsertPersonalInfo(user);
+            return Ok();
+        }
         [HttpGet]
         [Route("GetUserInfoByEmail/{email}")]
         public IHttpActionResult GetUserInfoByEmail(string email)
@@ -74,6 +92,17 @@ namespace Web.Controllers
                 return BadRequest("One or more required objects was not included in the request body.");
             }
             setUserInfoEngine.UpdatePaymentInfo(userPaymentInfo);
+            return Ok();
+        }
+        [HttpPost]
+        [Route("InsertPaymentInfo")]
+        public IHttpActionResult InsertPaymentInfo(UserPaymentInfoDTO userPaymentInfo)
+        {
+            if (userPaymentInfo.FirstName == null || userPaymentInfo.LastName == null || userPaymentInfo.State == null || userPaymentInfo.StreetAddress1 == null || userPaymentInfo.StreetAddress2 == null || userPaymentInfo.Zip == null)
+            {
+                return BadRequest("One or more required objects was not included in the request body.");
+            }
+            setUserInfoEngine.InsertPaymentInfo(userPaymentInfo);
             return Ok();
         }
     }
