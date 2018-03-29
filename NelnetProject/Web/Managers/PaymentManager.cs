15f39798b265f0bc664db4b5b3a8a9878132251d
Joe Cowman <joe.cowman@huskers.unl.edu>
2018-03-28 21:54:36 -0500

    Recurring timer

--- NelnetProject/Web/Managers/PaymentManager.cs
+++ NelnetProject/Web/Managers/PaymentManager.cs
@@ -3,12 +3,15 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Timers;
 using System.Web;
 
 namespace Web.Managers
 {
     public class PaymentManager
     {
+        static double timerInterval = 1000; //todo move
+
         IGetTransactionEngine getTransactionEngine;
         IPaymentEngine paymentEngine;
         INotificationEngine notificationEngine;
@@ -18,6 +21,15 @@ namespace Web.Managers
             this.getTransactionEngine = getTransactionEngine;
             this.paymentEngine = paymentEngine;
             this.notificationEngine = notificationEngine;
+
+            Timer timer = new Timer(timerInterval);
+            timer.Elapsed += new ElapsedEventHandler(TimerIntervalElapsed);
+            timer.Enabled = true;
+        }
+
+        public void TimerIntervalElapsed(object sender, ElapsedEventArgs e)
+        {
+            Debug.WriteLine("time elapsed");
         }
     }
 }
