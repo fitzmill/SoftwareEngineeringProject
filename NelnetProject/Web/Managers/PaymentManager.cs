6170845553aa8631f82fbf34233ff87548628c88
Joe Cowman <joe.cowman@huskers.unl.edu>
2018-03-28 22:25:04 -0500

    Scheduled events run on given day & hour

--- NelnetProject/Web/Managers/PaymentManager.cs
+++ NelnetProject/Web/Managers/PaymentManager.cs
@@ -1,4 +1,5 @@
-﻿using Core.Interfaces;
+﻿using Core;
+using Core.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -11,6 +12,7 @@ namespace Web.Managers
     public class PaymentManager
     {
         static double timerInterval = 1000; //todo move
+        static int chargingHour = 10; //todo move, change
 
         IGetTransactionEngine getTransactionEngine;
         IPaymentEngine paymentEngine;
@@ -30,6 +32,23 @@ namespace Web.Managers
         public void TimerIntervalElapsed(object sender, ElapsedEventArgs e)
         {
             Debug.WriteLine("time elapsed");
+            DateTime today = dateProvider();
+            if (today.Hour == chargingHour && today.Day == 1)
+            {
+                IList<Transaction> generatedTransactions = paymentEngine.GeneratePayments(today);
+                //notificationEngine.SendTransactionNotifications(generatedTransactions.ToList()); todo
+            }
+            else if (today.Hour == chargingHour && today.Day > 1 && today.Day <= 12)
+            {
+                IList<Transaction> unsettledTransactions = getTransactionEngine.GetAllUnsettledTransactions();
+                IList<Transaction> transactionResults = paymentEngine.ChargePayments(unsettledTransactions.ToList(), today);
+                //notificationEngine.SendTransactionNotifications(transactionResults.ToList()); todo
+            }
+        }
+        
+        DateTime dateProvider()
+        {
+            return DateTime.Today;
         }
     }
 }
