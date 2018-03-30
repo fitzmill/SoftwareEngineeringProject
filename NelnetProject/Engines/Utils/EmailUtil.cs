cca617b52588e60d46a48a77d16b687e3827da23
Joe Cowman <joe.cowman@huskers.unl.edu>
2018-03-30 16:34:07 -0500

    Started unit tests for EmailUtil

--- NelnetProject/Engines/Utils/EmailUtil.cs
+++ NelnetProject/Engines/Utils/EmailUtil.cs
@@ -14,16 +14,16 @@ namespace Engines.Utils
         //Generates email notification for an upcoming payment
         public static EmailNotification UpcomingPaymentNotification(Transaction t, User user)
         {
-            String subject = "Alert from Tuition Assistant: Upcoming Payment";
-            String rawBody = String.Format("You have an upcoming payment.\nDate: {0:MMMM d yyyy}\nAmount: ${1}\nYou don't need to worry about anything. We'll charge your card automatically on this date.", t.DateDue, t.AmountCharged);
+            string subject = "Alert from Tuition Assistant: Upcoming Payment";
+            string rawBody = string.Format("You have an upcoming payment.\nDate: {0:MMMM d yyyy}\nAmount: ${1}\nYou don't need to worry about anything. We'll charge your card automatically on this date.", t.DateDue, t.AmountCharged);
             return GenerateEmail(user.Email, subject, rawBody, user.FirstName);
         }
 
         //Generates email notification for a successfully charged payment
         public static EmailNotification PaymentChargedSuccessfullyNotification(Transaction t, User user)
         {
-            String subject = "Alert from Tuition Assistnat: Payment Successful";
-            String rawBody = String.Format("Congratulations! Your payment was processed succesfully.\nDate: {0:MMMM d yyyy}\nAmount: ${1}", t.DateCharged, t.AmountCharged);
+            string subject = "Alert from Tuition Assistant: Payment Successful";
+            string rawBody = string.Format("Congratulations! Your payment was processed succesfully.\nDate: {0:MMMM d yyyy}\nAmount: ${1}", t.DateCharged, t.AmountCharged);
             return GenerateEmail(user.Email, subject, rawBody, user.FirstName);
         }
 
@@ -31,8 +31,8 @@ namespace Engines.Utils
         public static EmailNotification PaymentUnsuccessfulRetryingNotification(Transaction t, User user, DateTime today)
         {
             int daysRemaining = TuitionUtil.DUE_DAY - TuitionUtil.DaysOverdue(t, today);
-            String subject = String.Format("Alert from Tuition Assistant: Payment Unsuccessful ({0} DAYS REMAINING)", daysRemaining);
-            String rawBody = String.Format("There was an issue with your credit card. Your payment on {0:MMMM d yyyy} for ${1} failed for the following reason: {2}." +
+            string subject = string.Format("Alert from Tuition Assistant: Payment Unsuccessful ({0} DAYS REMAINING)", daysRemaining);
+            string rawBody = string.Format("There was an issue with your credit card. Your payment on {0:MMMM d yyyy} for ${1} failed for the following reason: {2}." +
                 "\nPlease resolve the issue as soon as possible.\nIf the payment remains unsuccessful after {3} more days, the amound will be deferred and a late fee of ${4} will be added.",
                 t.DateCharged, t.AmountCharged, t.ReasonFailed, daysRemaining, TuitionUtil.LATE_FEE);
             return GenerateEmail(user.Email, subject, rawBody, user.FirstName);
@@ -41,16 +41,16 @@ namespace Engines.Utils
         //Generates email notification for an unsuccessful payment that has been deferred
         public static EmailNotification PaymentFailedNotification(Transaction t, User user)
         {
-            String subject = "Alert from Tuition Assistant: Payment Failed";
-            String rawBody = String.Format("Your payment of ${0} that was due on {1:MMMM d yyyy} failed for {2} days and has been deferred." +
+            string subject = "Alert from Tuition Assistant: Payment Failed";
+            string rawBody = string.Format("Your payment of ${0} that was due on {1:MMMM d yyyy} failed for {2} days and has been deferred." +
                 "\nThe amount will be added to your next payment, along with a late fee of ${3}.", t.AmountCharged, t.DateDue, TuitionUtil.OVERDUE_RETRY_PERIOD, TuitionUtil.LATE_FEE);
             return GenerateEmail(user.Email, subject, rawBody, user.FirstName);
         }
 
         //Generates email notification with the default Tuition Assistant body template
-        public static EmailNotification GenerateEmail(String to, String subject, String rawBody, String userFirstName)
+        public static EmailNotification GenerateEmail(string to, string subject, string rawBody, string userFirstName)
         {
-            String body = String.Format("Hi {0},\n{1}\nPlease contact us if you have any questions.\nPowered by Tuition Assistant\n", userFirstName, rawBody);
+            string body = string.Format("Hi {0},\n{1}\nPlease contact us if you have any questions.\nPowered by Tuition Assistant\n", userFirstName, rawBody);
             return new EmailNotification()
             {
                 To = to,
---
+++ NelnetProject/NelnetProject.Tests/Engines/Utils/TestEmailUtil.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Core;
+using Core.Models;
+using Engines.Utils;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NelnetProject.Tests.Engines.Utils
+{
+    [TestClass]
+    public class TestEmailUtil
+    {
+        [TestMethod]
+        public void TestGenerateEmail()
+        {
+            String to = "to@email";
+            String subject = "subject";
+            String rawBody = "This is a test line.";
+            String userFirstName = "Bob";
+            String expectedBody = "Hi Bob,\nThis is a test line.\nPlease contact us if you have any questions.\nPowered by Tuition Assistant\n";
+
+            EmailNotification email = EmailUtil.GenerateEmail(to, subject, rawBody, userFirstName);
+
+            Assert.AreEqual(to, email.To);
+            Assert.AreEqual(subject, email.Subject);
+            Assert.AreEqual(expectedBody, email.Body);
+        }
+
+        [TestMethod]
+        public void TestUpcomingPaymentNotification()
+        {
+            Transaction t = new Transaction() {
+                AmountCharged = 37.34,
+                DateDue = new DateTime(2018, 3, 30)
+            };
+            User u = new User() {
+                FirstName = "Bob",
+                Email = "bob@email.com"
+            };
+            String expectedBody = "Hi Bob,\nYou have an upcoming payment.\nDate: March 30 2018\nAmount: $37.34\nYou don't need to worry about anything. We'll charge your card automatically on this date.\nPlease contact us if you have any questions.\nPowered by Tuition Assistant\n";
+
+            EmailNotification email = EmailUtil.UpcomingPaymentNotification(t, u);
+
+            Assert.AreEqual(u.Email, email.To);
+            Assert.AreEqual("Alert from Tuition Assistant: Upcoming Payment", email.Subject);
+            Assert.AreEqual(expectedBody, email.Body);
+        }
+
+        [TestMethod]
+        public void TestPaymentChargedSuccessfullyNotification()
+        {
+            Transaction t = new Transaction()
+            {
+                AmountCharged = 37,
+                DateDue = new DateTime(2018, 3, 1),
+                DateCharged = new DateTime(2018, 3, 3),
+                ProcessState = ProcessState.SUCCESSFUL
+            };
+            User u = new User()
+            {
+                FirstName = "Bob",
+                Email = "bob@email.com"
+            };
+            String expectedBody = "Hi Bob,\nCongratulations! Your payment was processed succesfully.\nDate: March 3 2018\nAmount: $37.00\nPlease contact us if you have any questions.\nPowered by Tuition Assistant\n";
+
+            EmailNotification email = EmailUtil.PaymentChargedSuccessfullyNotification(t, u);
+
+            Assert.AreEqual(u.Email, email.To);
+            Assert.AreEqual("Alert from Tuition Assistant: Payment Successful", email.Subject);
+            Assert.AreEqual(expectedBody, email.Body);
+        }
+    }
+}
