550e4a71aa5b5878f6f9bc80d140f16e9e0f1f90
Joe Cowman <joe.cowman@huskers.unl.edu>
2018-04-04 21:55:46 -0500

    Added null checks in emailutil

--- NelnetProject/Engines/Utils/EmailUtil.cs
+++ NelnetProject/Engines/Utils/EmailUtil.cs
@@ -14,6 +14,15 @@ namespace Engines.Utils
         //Generates email notification for an upcoming payment
         public static EmailNotification UpcomingPaymentNotification(Transaction t, User user)
         {
+            if (t == null)
+            {
+                throw new ArgumentNullException("Transaction cannot be null.");
+            }
+            if (user == null)
+            {
+                throw new ArgumentNullException("User cannot be null.");
+            }
+
             string subject = "Alert from Tuition Assistant: Upcoming Payment";
             string rawBody = string.Format("You have an upcoming payment.\nDate: {0:MMMM d yyyy}\nAmount: ${1:f2}\nYou don't need to worry about anything. We'll charge your card automatically on this date.", t.DateDue, t.AmountCharged);
             return GenerateEmail(user.Email, subject, rawBody, user.FirstName);
@@ -22,6 +31,15 @@ namespace Engines.Utils
         //Generates email notification for a successfully charged payment
         public static EmailNotification PaymentChargedSuccessfullyNotification(Transaction t, User user)
         {
+            if (t == null)
+            {
+                throw new ArgumentNullException("Transaction cannot be null.");
+            }
+            if (user == null)
+            {
+                throw new ArgumentNullException("User cannot be null.");
+            }
+
             string subject = "Alert from Tuition Assistant: Payment Successful";
             string rawBody = string.Format("Congratulations! Your payment was processed succesfully.\nDate: {0:MMMM d yyyy}\nAmount: ${1:f2}", t.DateCharged, t.AmountCharged);
             return GenerateEmail(user.Email, subject, rawBody, user.FirstName);
@@ -30,6 +48,15 @@ namespace Engines.Utils
         //Generates email notification for an unsuccessful payment that is being retried
         public static EmailNotification PaymentUnsuccessfulRetryingNotification(Transaction t, User user, DateTime today)
         {
+            if (t == null)
+            {
+                throw new ArgumentNullException("Transaction cannot be null.");
+            }
+            if (user == null)
+            {
+                throw new ArgumentNullException("User cannot be null.");
+            }
+
             int daysRemaining = TuitionUtil.DUE_DAY - TuitionUtil.DaysOverdue(t, today);
             string subject = string.Format("Alert from Tuition Assistant: Payment Unsuccessful ({0} DAYS REMAINING)", daysRemaining);
             string rawBody = string.Format("There was an issue with your credit card. Your payment on {0:MMMM d yyyy} for ${1:f2} failed for the following reason: {2}." +
@@ -41,6 +68,15 @@ namespace Engines.Utils
         //Generates email notification for an unsuccessful payment that has been deferred
         public static EmailNotification PaymentFailedNotification(Transaction t, User user)
         {
+            if (t == null)
+            {
+                throw new ArgumentNullException("Transaction cannot be null.");
+            }
+            if (user == null)
+            {
+                throw new ArgumentNullException("User cannot be null.");
+            }
+
             string subject = "Alert from Tuition Assistant: Payment Failed";
             string rawBody = string.Format("Your payment of ${0:f2} that was due on {1:MMMM d yyyy} failed for {2} days and has been deferred." +
                 "\nThe amount will be added to your next payment, along with a late fee of ${3:f2}.", t.AmountCharged, t.DateDue, TuitionUtil.OVERDUE_RETRY_PERIOD, TuitionUtil.LATE_FEE);
