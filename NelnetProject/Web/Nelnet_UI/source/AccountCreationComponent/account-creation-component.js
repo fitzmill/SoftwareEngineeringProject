f62ae5337ac976358c8443653c957ade98d41117
LHall383 <lahall1009@aol.com>
2018-04-09 22:17:20 -0500

    Fixed issues from pull request

--- NelnetProject/Web/Nelnet_UI/source/AccountCreationComponent/account-creation-component.js
+++ NelnetProject/Web/Nelnet_UI/source/AccountCreationComponent/account-creation-component.js
@@ -1,6 +1,10 @@
 ﻿require('./account-creation-component.scss');
 require('../assets/background-image.scss');
 
+//constants
+const START_PAGE = 1;
+const END_PAGE = 4;
+
 ko.components.register('account-creation-component', {
     viewModel: function (params) {
         let vm = this;
@@ -35,6 +39,7 @@ ko.components.register('account-creation-component', {
 
         vm.addStudent = function () {
             vm.students.push({
+                studentID: 0,
                 studentFirstName: ko.observable(),
                 studentLastName: ko.observable(),
                 studentGrade: ko.observable()
@@ -45,16 +50,14 @@ ko.components.register('account-creation-component', {
         vm.paymentType = ko.observable();
         vm.calcYearlyRate = function () {
             return 2640;
-        }
+        };
 
         //keep track of page state
-        vm.currentPage = 1;
-        vm.START_PAGE = 1;
-        vm.END_PAGE = 4;
+        vm.currentPage = START_PAGE;
 
         //create function to update progress bar
         vm.updateProgressBar = function () {
-            for (let i = vm.START_PAGE; i < vm.END_PAGE; i++) {
+            for (let i = START_PAGE; i < END_PAGE; i++) {
                 if (i >= vm.currentPage) {
                     $('#rectangle-' + i)[0].style.backgroundColor = "#afafaf";
                 } else {
@@ -62,37 +65,37 @@ ko.components.register('account-creation-component', {
                 }
             }
 
-            for (let i = vm.START_PAGE; i <= vm.END_PAGE; i++) {
+            for (let i = START_PAGE; i <= END_PAGE; i++) {
                 if (i > vm.currentPage) {
                     $('#dot-page-' + i)[0].style.backgroundColor = "#afafaf";
                 } else {
                     $('#dot-page-' + i)[0].style.backgroundColor = "#007bff";
                 }
             }
-        }
+        };
 
         //create function to update buttons
         vm.updateButtons = function () {
-            if (vm.currentPage === vm.START_PAGE) {
+            if (vm.currentPage === START_PAGE) {
                 $("#btn-cancel").show();
                 $("#btn-back").hide();
             } else {
                 $("#btn-cancel").hide();
                 $("#btn-back").show();
             }
-            if (vm.currentPage === vm.END_PAGE) {
+            if (vm.currentPage === END_PAGE) {
                 $("#btn-done").show();
                 $("#btn-next").hide();
             } else {
                 $("#btn-done").hide();
                 $("#btn-next").show();
             }
-        }
+        };
 
         //exit account creation
         vm.cancel = function () {
             window.location = '#';
-        }
+        };
 
         //move to the previous page
         vm.back = function () {
@@ -101,7 +104,7 @@ ko.components.register('account-creation-component', {
             $("#info-page-" + vm.currentPage).show();
             vm.updateButtons();
             vm.updateProgressBar();
-        }
+        };
 
         //move to the next page
         vm.next = function () {
@@ -110,15 +113,15 @@ ko.components.register('account-creation-component', {
             $("#info-page-" + vm.currentPage).show();
             vm.updateButtons();
             vm.updateProgressBar();
-        }
+        };
 
         //finish and create account
         vm.done = function () {
-
-        }
+            //TODO
+        };
 
         //show current page and correct buttons
-        for (let i = vm.START_PAGE; i <= vm.END_PAGE; i++) {
+        for (let i = START_PAGE; i <= END_PAGE; i++) {
             if (i === vm.currentPage) {
                 $("#info-page-" + i).show();
             } else {
