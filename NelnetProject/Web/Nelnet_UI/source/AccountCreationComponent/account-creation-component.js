9199fcf094c4b052f35f6c67b87e269041062577
LHall383 <lahall1009@aol.com>
2018-04-11 20:04:13 -0500

    Got ajax call to controller working

--- NelnetProject/Web/Nelnet_UI/source/AccountCreationComponent/account-creation-component.js
+++ NelnetProject/Web/Nelnet_UI/source/AccountCreationComponent/account-creation-component.js
@@ -1,9 +1,12 @@
 ﻿require('./account-creation-component.scss');
 require('../assets/background-image.scss');
 
-//constants
+//paging constants
 const START_PAGE = 1;
 const END_PAGE = 4;
+
+//api url constants
+const accountCreationAPIURL = "/api/account";
 
 ko.components.register('account-creation-component', {
     viewModel: function (params) {
@@ -32,6 +35,7 @@ ko.components.register('account-creation-component', {
 
         //third page
         vm.students = ko.observableArray([{
+            studentID: 0,
             studentFirstName: ko.observable(),
             studentLastName: ko.observable(),
             studentGrade: ko.observable()
@@ -48,9 +52,22 @@ ko.components.register('account-creation-component', {
 
         //fourth page
         vm.paymentType = ko.observable();
-        vm.calcYearlyRate = function () {
-            return 2640;
+        vm.calcRate = function (planType) {
+            let students = vm.students().map(s => {
+                return {
+                    Grade: s.studentGrade()
+                };
+            });
+            calculatePeriodicPayment(planType, students).done(function (data) {
+                console.log(data);
+                return data;
+            }).fail(function (jqXHR) {
+                window.alert("Could not calculate rate.");
+            });
         };
+        vm.yearlyRate = ko.observable(0);
+        vm.monthlyRate = ko.observable(0);
+        vm.semesterlyRate = ko.observable(0);
 
         //keep track of page state
         vm.currentPage = START_PAGE;
@@ -89,6 +106,12 @@ ko.components.register('account-creation-component', {
             } else {
                 $("#btn-done").hide();
                 $("#btn-next").show();
+            }
+            //radio buttons
+            if (vm.currentPage == END_PAGE) {
+                vm.yearlyRate = vm.calcRate("YEARLY");
+                vm.semesterlyRate = vm.calcRate("SEMESTERLY");
+                vm.monthlyRate = vm.calcRate("MONTHLY");
             }
         };
 
@@ -136,3 +159,25 @@ ko.components.register('account-creation-component', {
 
     template: require('./account-creation-component.html')
 });
+
+//calculate the user's payment info
+function calculatePeriodicPayment(userPaymentPlan, students) {
+    let user = JSON.stringify({
+        Plan: userPaymentPlan,
+        Students: students
+    });
+    return $.ajax(accountCreationAPIURL + "/CalculatePeriodicPayment", {
+        method: "POST",
+        contentType: "application/json; charset=utf-8",
+        data: user
+    });
+}
+
+Number.prototype.formatMoney = function (decimals) {
+    let number = this;
+    sign = number < 0 ? "-" : "";
+    number = Math.abs(Number(number) || 0).toFixed(decimals);
+    i = String(parseInt(number));
+    j = (j = i.length) > 3 ? j % 3 : 0;
+    return sign + (j ? i.substr(0, j) + "," : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + ",") + (decimals ? "." + Math.abs(n - i).toFixed(decimals).slice(2) : "");
+};
