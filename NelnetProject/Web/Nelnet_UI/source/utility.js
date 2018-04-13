faf053137f4c43f072e2cf03a745b095995a6547
fitzmill <sfitzmill@gmail.com>
2018-04-12 22:07:41 -0500

    fixed styling errors in firefox

--- NelnetProject/Web/Nelnet_UI/source/utility.js
+++ NelnetProject/Web/Nelnet_UI/source/utility.js
@@ -62,7 +62,7 @@ String.prototype.passwordMeetsRequirements = function () {
 
 $.validator.addMethod("password", function (value) {
     return value.match(/^(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-])(?!.*?[;'"]).{8,32}$/);
-}, "Password must be between 8 and 32 letters, contain lowercase and uppercase letters, a number, and a special character");
+}, "Password must be between 8 and 32 letters, contain lowercase and uppercase letters, a number, and a special character.");
 
 String.prototype.emailMeetsRequirements = function () {
     //from emailregex.com
