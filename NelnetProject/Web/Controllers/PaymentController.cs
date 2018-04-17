9feb2509f0a6f02ccf7839272d7ee0fce1ddf926
Joe Cowman <joe.cowman@huskers.unl.edu>
2018-04-16 23:53:40 -0500

    Cleaned up PaymentController

--- NelnetProject/Web/Controllers/PaymentController.cs
+++ NelnetProject/Web/Controllers/PaymentController.cs
@@ -1,51 +1,74 @@
 ﻿using Core;
+using Core.Interfaces;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Security.Claims;
 using System.Web.Http;
 using Web.Filters;
 
 namespace Web.Controllers
 {
+    /// <summary>
+    /// Controls methods for retrieving and calculating payments (a.k.a. transactions).
+    /// </summary>
     [RoutePrefix("api/payment")]
     [SqlRowNotAffectedFilter]
     public class PaymentController : ApiController
     {
+        IGetTransactionEngine _getTransactionEngine;
+        IPaymentEngine _paymentEngine;
 
-        //This is a get request with the above route. The 5 at the end of the example is an example userID
+        public PaymentController(IGetTransactionEngine getTransactionEngine, IPaymentEngine paymentEngine)
+        {
+            _getTransactionEngine = getTransactionEngine;
+            _paymentEngine = paymentEngine;
+        }
+
+        /// <summary>
+        /// Gets all transactions for the given user.
+        /// </summary>
+        /// <returns>A list of all of the user's transactions</returns>
         [HttpGet]
         [Route("GetAllTransactionsForUser")]
         [JwtAuthentication]
         public IHttpActionResult GetAllTransactionsForUser()
         {
-            var user = (ClaimsIdentity)User.Identity;
+            var user = (ClaimsIdentity) User.Identity;
             var userID = user.Claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier)?.Value;
+
             if (!int.TryParse(userID, out int parsedUserID))
             {
                 return BadRequest("Could not parse userID into an integer");
             }
-            return Ok(getTransactionEngine.GetAllTransactionsForUser(parsedUserID));
+
+            return Ok(_getTransactionEngine.GetAllTransactionsForUser(parsedUserID));
         }
 
-        //GET api/account/GetNextTransactionForUser/{userID}
+        /// <summary>
+        /// Gets the next payment for the given user.
+        /// </summary>
+        /// <returns>The next payment due for the user</returns>
         [HttpGet]
-        [Route("GetNextTransactionForUser")]
+        [Route("GetNextPaymentForUser")]
         [JwtAuthentication]
-        public IHttpActionResult GetNextTransactionForUser()
+        public IHttpActionResult GetNextPaymentForUser()
         {
             var user = (ClaimsIdentity)User.Identity;
             var userID = user.Claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier)?.Value;
+
             if (!int.TryParse(userID, out int parsedUserID))
             {
                 return BadRequest("Could not parse userID into an integer");
             }
-            return Ok(paymentEngine.CalculateNextPaymentForUser(parsedUserID, DateTime.Now));
+
+            return Ok(_paymentEngine.CalculateNextPaymentForUser(parsedUserID, DateTime.Now));
         }
 
-        //POST api/account/CalculatePeriodicPayment
+        /// <summary>
+        /// Calculate what the periodic payment will be for the given user.
+        /// </summary>
+        /// <param name="user">the user</param>
+        /// <returns>the amount due each period</returns>
         [HttpPost]
         [Route("CalculatePeriodicPayment")]
         [AllowAnonymous]
@@ -55,7 +78,8 @@ namespace Web.Controllers
             {
                 return BadRequest("One or more required objects was not included in the request body.");
             }
-            return Ok(paymentEngine.CalculatePeriodicPayment(user));
+
+            return Ok(_paymentEngine.CalculatePeriodicPayment(user));
         }
 
     }
