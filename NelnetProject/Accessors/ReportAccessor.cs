ad840b8895f759858531afa8b56546c54d277fb5
Joe Cowman <joe.cowman@huskers.unl.edu>
2018-04-19 19:34:22 -0500

    Added SqlConnectionFactory

--- NelnetProject/Accessors/ReportAccessor.cs
+++ NelnetProject/Accessors/ReportAccessor.cs
@@ -1,5 +1,6 @@
 ﻿using Core.Interfaces.Accessors;
 using Core.Models;
+using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 
@@ -14,25 +15,17 @@ namespace Accessors
             _connectionString = connectionString;
         }
 
+        private readonly string _getAllReportsQuery = "SELECT * FROM [dbo].[Report] r ORDER BY r.ReportID DESC";
+        private readonly string _insertReportQuery = "INSERT INTO * [dbo].[Report] (DateCreated, StartDate, EndDate) VALUES (@DateCreated, @StartDate, @EndDate)";
+
         public IEnumerable<Report> GetAllReports()
         {
-            string query = "[dbo].[GetAllReports]";
+            List<Report> result = new List<Report>();
 
-            var result = new List<Report>();
-
-            using (SqlConnection conn = new SqlConnection(_connectionString))
-            {
-                SqlCommand command = new SqlCommand(query, conn);
-
-                command.CommandType = System.Data.CommandType.StoredProcedure;
-
-                conn.Open();
-
-                var reader = command.ExecuteReader();
-
+            SqlConnectionFactory.RunSqlQuery(_getAllReportsQuery, reader => {
                 while (reader.Read())
                 {
-                    result.Add(new Report()
+                    result.Add(new Report
                     {
                         ReportID = reader.GetInt32(0),
                         DateCreated = reader.GetDateTime(1),
@@ -40,33 +33,99 @@ namespace Accessors
                         EndDate = reader.GetDateTime(3)
                     });
                 }
-            }
+            });
 
             return result;
+
+            //using (SqlConnection con = SqlConnectionFactory.CreateConnection())
+            //{
+            //    using (SqlCommand command = new SqlCommand(_getAllReportsQuery, con))
+            //    {
+            //        using (SqlDataReader reader = command.ExecuteReader())
+            //        {
+            //            while (reader.Read())
+            //            {
+            //                result.Add(new Report
+            //                {
+            //                    ReportID = reader.GetInt32(0),
+            //                    DateCreated = reader.GetDateTime(1),
+            //                    StartDate = reader.GetDateTime(2),
+            //                    EndDate = reader.GetDateTime(3)
+            //                });
+            //            }
+            //        }
+            //    }
+            //}
+
+            //return result;
         }
 
         public void InsertReport(Report report)
         {
-            string query = "[dbo].[InsertReport]";
-
-            using (SqlConnection conn = new SqlConnection(_connectionString))
+            using (SqlConnection con = SqlConnectionFactory.CreateConnection())
             {
-                var command = new SqlCommand(query, conn);
-
-                command.Parameters.Add(new SqlParameter("@StartDate", report.StartDate));
-                command.Parameters.Add(new SqlParameter("@EndDate", report.EndDate));
-
-                command.CommandType = System.Data.CommandType.StoredProcedure;
-
-                conn.Open();
-
-                var reader = command.ExecuteReader();
-
-                if (reader.Read())
+                using (SqlCommand command = new SqlCommand(_insertReportQuery, con))
                 {
-                    report.ReportID = (int)reader.GetDecimal(0);
+                    command.Parameters.Add(new SqlParameter("@DateCreated", DateTime.Today));
+                    command.Parameters.Add(new SqlParameter("@StartDate", report.StartDate));
+                    command.Parameters.Add(new SqlParameter("@EndDate", report.EndDate));
                 }
             }
         }
+
+        //public IEnumerable<Report> GetAllReports()
+        //{
+        //    string query = "[dbo].[GetAllReports]";
+
+        //    var result = new List<Report>();
+
+        //    using (SqlConnection conn = new SqlConnection(_connectionString))
+        //    {
+        //        SqlCommand command = new SqlCommand(query, conn);
+
+        //        command.CommandType = System.Data.CommandType.StoredProcedure;
+
+        //        conn.Open();
+
+        //        var reader = command.ExecuteReader();
+
+        //        while (reader.Read())
+        //        {
+        //            result.Add(new Report()
+        //            {
+        //                ReportID = reader.GetInt32(0),
+        //                DateCreated = reader.GetDateTime(1),
+        //                StartDate = reader.GetDateTime(2),
+        //                EndDate = reader.GetDateTime(3)
+        //            });
+        //        }
+        //    }
+
+        //    return result;
+        //}
+
+        //public void InsertReport(Report report)
+        //{
+        //    string query = "[dbo].[InsertReport]";
+
+        //    using (SqlConnection conn = new SqlConnection(_connectionString))
+        //    {
+        //        var command = new SqlCommand(query, conn);
+
+        //        command.Parameters.Add(new SqlParameter("@StartDate", report.StartDate));
+        //        command.Parameters.Add(new SqlParameter("@EndDate", report.EndDate));
+
+        //        command.CommandType = System.Data.CommandType.StoredProcedure;
+
+        //        conn.Open();
+
+        //        var reader = command.ExecuteReader();
+
+        //        if (reader.Read())
+        //        {
+        //            report.ReportID = (int)reader.GetDecimal(0);
+        //        }
+        //    }
+        //}
     }
 }
---
+++ NelnetProject/Accessors/SqlConnectionFactory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accessors
+{
+    public class SqlConnectionFactory
+    {
+        private static readonly string _connectionString = ConfigurationManager.ConnectionStrings["NelnetPaymentProcessing"].ConnectionString;
+
+        public static SqlConnection CreateConnection()
+        {
+            SqlConnection con = new SqlConnection(_connectionString);
+            con.Open();
+            return con;
+        }
+
+        public static void RunSqlQuery(string query, Action<SqlDataReader> readFunc)
+        {
+            using (SqlConnection con = CreateConnection())
+            {
+                using (SqlCommand command = new SqlCommand(query, con))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        readFunc(reader);
+                    }
+                }
+            }
+        }
+    }
+}
